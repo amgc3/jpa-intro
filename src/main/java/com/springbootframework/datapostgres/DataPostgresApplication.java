1d4dbe036fa1ccca9219371db2fae5b394650e70
Anna Cotugno <annacotugno@Annas-MacBook-Pro.local>
2022-02-11 14:33:23 +0000

    first commit

---
+++ src/main/java/com/springbootframework/datapostgres/DataPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.springbootframework.datapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootframework/datapostgres/DataPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootframework.datapostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
