9691ec51e91e1e13d223ddb2dfe4289e4ef03056
Anna Cotugno <annacotugno@Annas-MacBook-Pro.local>
2022-02-16 13:19:05 +0000

    add file to initialise database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,48 @@
+INSERT INTO cities (city_name, country_code) VALUES ('London', 'GB');
+INSERT INTO cities (city_name, country_code)  VALUES ('Grayshott', 'GB');
+INSERT INTO cities (city_name, country_code)  VALUES ('New York City', 'US');
+INSERT INTO cities (city_name, country_code)  VALUES ('Las Vegas', 'US');
+INSERT INTO cities (city_name, country_code)  VALUES ('Los Angeles', 'US');
+INSERT INTO cities (city_name, country_code)  VALUES ('Paris', 'FR');
+
+
+-- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+-- VALUES('Jude','Law','M',1972,1);
+-- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+-- VALUES('Scarlett','Johansson','F',1984,3);
+-- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+-- VALUES('Colin','Firth','M',1960,2);
+-- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+-- VALUES('Leonardo','DiCaprio','M',1974,5);
+-- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+-- VALUES('Jean','Gabin','F',1974,6);
+-- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+-- VALUES('Audrey','Tautou','F',1976,6);
+-- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+-- VALUES('Joaquin','Phoenix','M',1974,5);
+
+INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+VALUES('Jude','Law','M',1972,1);
+INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+VALUES('Scarlett','Johansson','F',1984,3);
+INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+VALUES('Colin','Firth','M',1960,2);
+INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+VALUES('Leonardo','DiCaprio','M',1974,5);
+INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+VALUES('Jean','Gabin','F',1974,6);
+INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+VALUES('Audrey','Tautou','F',1976,6);
+INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
+VALUES('Joaquin','Phoenix','M',1974,5);
+
+INSERT INTO films (title, release_year, language, runtime_minutes)
+VALUES('The Kings Speech',2010,'en', 118);
+INSERT INTO films (title, release_year, language, runtime_minutes)
+VALUES('Lord of the Rings',2004,'en',130);
+INSERT INTO films (title, release_year, language, runtime_minutes)
+VALUES('Under the Skin',2013,'en',108);
+INSERT INTO films (title, release_year, language, runtime_minutes)
+VALUES('My Blueberry Nights',2007,'en',95);
+INSERT INTO films (title, release_year, language, runtime_minutes)
+VALUES('Port of Shadows',1938,'fr',128);
