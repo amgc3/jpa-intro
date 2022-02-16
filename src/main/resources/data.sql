d1863a41c09ad98e1b2848ad79545d40acc193c1
Anna Cotugno <annacotugno@Annas-MacBook-Pro.local>
2022-02-16 13:22:18 +0000

    delete comments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,22 +5,6 @@ INSERT INTO cities (city_name, country_code)  VALUES ('Las Vegas', 'US');
 INSERT INTO cities (city_name, country_code)  VALUES ('Los Angeles', 'US');
 INSERT INTO cities (city_name, country_code)  VALUES ('Paris', 'FR');
 
-
--- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
--- VALUES('Jude','Law','M',1972,1);
--- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
--- VALUES('Scarlett','Johansson','F',1984,3);
--- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
--- VALUES('Colin','Firth','M',1960,2);
--- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
--- VALUES('Leonardo','DiCaprio','M',1974,5);
--- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
--- VALUES('Jean','Gabin','F',1974,6);
--- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
--- VALUES('Audrey','Tautou','F',1976,6);
--- INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
--- VALUES('Joaquin','Phoenix','M',1974,5);
-
 INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
 VALUES('Jude','Law','M',1972,1);
 INSERT INTO actors (first_name, last_name, gender, birth_year, city_id)
