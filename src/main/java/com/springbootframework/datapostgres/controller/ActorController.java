ff9f9c9cfe86259dc7695740f2626c8ea9cb1efb
amgc3 <anna.cotugno@deltacapita.com>
2022-02-25 17:38:31 +0000

    use Actor DAO with ModelMapper

--- src/main/java/com/springbootframework/datapostgres/controller/ActorController.java
+++ src/main/java/com/springbootframework/datapostgres/controller/ActorController.java
@@ -1,10 +1,11 @@
 package com.springbootframework.datapostgres.controller;
 
-import com.springbootframework.datapostgres.exception.ResourceNotFoundException;
+import com.springbootframework.datapostgres.dto.ActorDTO;
 import com.springbootframework.datapostgres.model.Actor;
 import com.springbootframework.datapostgres.model.Film;
 import com.springbootframework.datapostgres.service.ActorService;
 import com.springbootframework.datapostgres.service.FilmService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +15,13 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/actors")
 public class ActorController {
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Autowired
     private final ActorService actorService;
@@ -29,48 +34,68 @@ public class ActorController {
         this.filmService = filmService;
     }
 
-    @GetMapping("/actors")
-    public ResponseEntity<List<Actor>> getActors() {
-        return new ResponseEntity<>(this.actorService.fetchActors(), HttpStatus.OK);
+//    @GetMapping
+//    public ResponseEntity<List<Actor>> getActors() {
+//        return new ResponseEntity<>(this.actorService.fetchActors(), HttpStatus.OK);
+//    }
+    @GetMapping
+    public ResponseEntity<List<ActorDTO>> getActors() {
+
+    return new ResponseEntity<>(this.actorService.fetchActors()
+            .stream()
+            .map(actor -> modelMapper.map(actor, ActorDTO.class))
+            .collect(Collectors.toList()), HttpStatus.OK);
     }
 
-    @GetMapping("/actors/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Object> getActorById(@PathVariable("id") Integer id) {
         Optional<Actor> actorOptional = this.actorService.fetchActorById(id);
         if (actorOptional.isPresent()) {
             Actor actor = actorOptional.get();
-            return new ResponseEntity<>(actor, HttpStatus.OK);
+            return new ResponseEntity<>(modelMapper.map(actor, ActorDTO.class), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(String.format("Invalid id : %s ", id), HttpStatus.NOT_FOUND);
         }
     }
 
-    @PutMapping("/actors/{id}")
-    public ResponseEntity<Object> updateActor(@PathVariable("id") Integer id, @RequestBody Actor a) {
-        Actor actorToUpdate = this.actorService.updateActorById(id, a);
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Object> updateActor(@PathVariable("id") Integer id, @RequestBody Actor a) {
+//        Actor actorToUpdate = this.actorService.updateActorById(id, a);
+//        if (actorToUpdate != null) {
+//            return new ResponseEntity<>(actorToUpdate, HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(String.format("Invalid id : %s ", id), HttpStatus.NOT_FOUND);
+//        }
+//    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateActor(@PathVariable("id") Integer id, @RequestBody ActorDTO actorDTO) {
+        // convert DTO to Entity
+        Actor actor = modelMapper.map(actorDTO, Actor.class);
+        Actor actorToUpdate = this.actorService.updateActorById(id, actor);
         if (actorToUpdate != null) {
-            return new ResponseEntity<>(actorToUpdate, HttpStatus.OK);
+            // return updatedActorDTO = modelMapper.map(actorToUpdate, ActorDTO.class)
+            return new ResponseEntity<>(modelMapper.map(actorToUpdate, ActorDTO.class), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(String.format("Invalid id : %s ", id), HttpStatus.NOT_FOUND);
         }
     }
 
-    @DeleteMapping("/actors/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteActor(@PathVariable("id") Integer id) {
         Actor actorToDelete = this.actorService.deleteActorById(id);
         if (actorToDelete != null) {
-            return new ResponseEntity<>(actorToDelete, HttpStatus.OK);
+            return new ResponseEntity<>(modelMapper.map(actorToDelete, ActorDTO.class), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(String.format("Invalid id : %s ", id), HttpStatus.NOT_FOUND);
         }
     }
 
-    @GetMapping("actors/films")
+    @GetMapping("/films")
     public ResponseEntity<List<Film>> getFilms() {
         return new ResponseEntity<>(this.filmService.fetchFilms(), HttpStatus.OK) ;
     }
 
-    @PostMapping("/actors")
+    @PostMapping
     public ResponseEntity<Actor> postActor(@Valid final @RequestBody Actor actor) {
         try {
             Actor savedActor = this.actorService.saveActor(actor);
@@ -82,7 +107,7 @@ public class ActorController {
         }
     }
 
-    @PostMapping("/actors/films")
+    @PostMapping("/films")
     public ResponseEntity<Film> postFilm(@Valid final @RequestBody Film film) {
         try {
             Film savedFilm = this.filmService.saveFilm(film);
