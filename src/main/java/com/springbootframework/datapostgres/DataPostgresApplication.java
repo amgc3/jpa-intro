a19631d6e83729421f372858931898929e1094b7
amgc3 <anna.cotugno@deltacapita.com>
2022-02-25 10:54:54 +0000

    add ModelMapper

--- src/main/java/com/springbootframework/datapostgres/DataPostgresApplication.java
+++ src/main/java/com/springbootframework/datapostgres/DataPostgresApplication.java
@@ -1,10 +1,18 @@
 package com.springbootframework.datapostgres;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DataPostgresApplication {
+
+	// configure a ModelMapper class as Bean so that it can be injected in the controller class
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataPostgresApplication.class, args);
