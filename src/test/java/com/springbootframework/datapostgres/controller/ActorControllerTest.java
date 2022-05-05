d7d89311dbf9fbd9994dcb08f764c9927e5841f6
amgc3 <anna.cotugno@deltacapita.com>
2022-05-05 17:33:20 +0100

    set up controller test

---
+++ src/test/java/com/springbootframework/datapostgres/controller/ActorControllerTest.java
@@ -0,0 +1,44 @@
+package com.springbootframework.datapostgres.controller;
+
+import com.springbootframework.datapostgres.dto.ActorDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ActorControllerTest {
+
+    @Test
+    void shouldReturnFullListOfActors(@Autowired TestRestTemplate template) {
+        ResponseEntity<List> entity = template.getForEntity("/actors", List.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, entity.getHeaders().getContentType());
+        List<ActorDTO> response = entity.getBody();
+        assertNotNull(response);
+        assertEquals(7, response.size());
+    }
+
+    @Test
+    void getActorById() {
+    }
+
+    @Test
+    void updateActor() {
+    }
+
+    @Test
+    void deleteActor() {
+    }
+
+    @Test
+    void postActor() {
+    }
+}
